18e85fae0a10aee6e4883ae48e826b7e5bf80859
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-02-08 01:38:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,8 +31,17 @@ class Song
    @@artists.uniq!
  end
  
-def genre_count
-  @@count 
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
 end
 
 # def artist_count
